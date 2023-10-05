e7d38d3559ed131f920e15c704027d809f8cd994
LiamHarrison25 <liam.harrison@mymail.champlain.edu>
2023-10-05 13:50:52 -0400

    Fixed the crontab checks
    All of the crontab checks now work as intended

--- Week5/secCheck.bash
+++ Week5/secCheck.bash
@@ -10,6 +10,7 @@ then
 
 	echo -e "\e[1;31mThe $1 is not compliant. The current policy should be: $2.\e[0m"
 	echo -e "\e[1;31mThe current value is: $3.\e[0m"
+	echo -e "\e[1;31mRemediation: $4.\e[0m"
 
 else
 
@@ -48,7 +49,7 @@ do
 done
 
 # Check IP forwarding is disables
-pforw=$(egrep -i "net\.ipv4\.ip_forward" /etc/sysctl.conf /etc/sysctl.d/* | awk ' { print $1 } ')
+pforw=$(egrep -i "net\.ipv4\.ip_forward" /etc/sysctl.conf /etc/sysctl.d/* | cut -d '=' -f 2 )
 checks "Ip Forwarding " "0" "${pforw}"
 
 # Check ICMP redirects are not accepted
@@ -56,51 +57,51 @@ picmp=$(egrep -i "net\.ipv4\.conf\.all\.accept_redirects" /etc/sysctl.conf | awk
 checks "ICMP redirects" "0" "${picmp}"
 
 # Check crontab is configured
-pcrontab=$(egrep -i "Access: (0700/--------) Uid: ()" stat /etc/crontab | awk ' { print $1} ')
-checks "crontab permissions" "0" "${pcrontab}"
+pcrontab=$(stat /etc/crontab | awk 'NR ==4 { print $4,$5,$6,$8,$9,$10 } ')
+checks "crontab permissions" "( 0/ root) ( 0/ root)" "${pcrontab}"
 
 # Check cron.hourly is configured
-pcronhour=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/cron.hourly | awk ' { print $1} ')
-checks "crontab hourly" "/0" "${pcronhour}"
+pcronhour=$(stat /etc/cron.hourly | awk 'NR ==4 { print $4,$5,$6,$8,$9,$10 } ')
+checks "crontab hourly" "( 0/ root) ( 0/ root)" "${pcronhour}"
 
 # Check cron.daily is configured
-pcrondail=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/cron.daily | awk ' { print $1} ')
-checks "crontab daily" "/0" "${pcrondail}"
+pcrondail=$(stat /etc/cron.daily | awk 'NR ==4 { print $4,$5,$6,$8,$9,$10 } ')
+checks "crontab daily" "( 0/ root) ( 0/ root)" "${pcrondail}"
 
 # Check cron.weekly is configured
-pcronweek=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/cron.weekly | awk ' { print $1} ')
-checks "crontab weekly" "/0" "${pcronweek}"
+pcronweek=$(stat /etc/cron.weekly | awk 'NR ==4 { print $4,$5,$6,$8,$9,$10 } ')
+checks "crontab weekly" "( 0/ root) ( 0/ root)" "${pcronweek}"
 
 # Check cron.monthly is configured
-pcronmont=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/cron.monthly | awk ' { print $1} ')
-checks "crontab monthly" "/0" "${pcronmont}"
+pcronmont=$(stat /etc/cron.monthly | awk 'NR ==4 { print $4,$5,$6,$8,$9,$10 } ')
+checks "crontab monthly" "( 0/ root) ( 0/ root)" "${pcronmont}"
 
 # Check passwd is configured
-ppasswd=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/passwd | awk ' { print $1} ')
+ppasswd=$(stat /etc/passwd | awk ' { print $1} ')
 checks "passwd" "0" "${ppasswd}"
 
 # Check shadow is configured
-pshadow=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/shadow | awk ' { print $1} ')
+pshadow=$(stat /etc/shadow | awk ' { print $1} ')
 checks "shadow" "0" "${pshadow}"
 
 # Check group is configured
-pgroup=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/group | awk ' { print $1} ')
+pgroup=$(stat /etc/group | awk ' { print $1} ')
 checks "group" "0" "${pgroup}"
 
 # Check gshadow is configured
-pgshadow=$(egrep "Access: (0700/-------)  Uid: ()" stat /etc/gshadow | awk ' { print $1} ')
+pgshadow=$(stat /etc/gshadow | awk ' { print $1} ')
 checks "gshadow" "0" "${pgshadow}"
 
 # Check no legacy + entries in passwd
-plegpass=$(grep '^\+:' /etc/passwd | awk ' { print $1} ')
+plegpass=$(grep '^\+:' /etc/passwd | awk ' { print s3} ')
 checks "legacy entries in passwd" "0" "${plegpass}"
 
 # Check no legacy + entries in shadow
-plegshad=$(grep '^\+:' /etc/shadow | awk ' { print $1} ')
+plegshad=$(grep '^\+:' /etc/shadow )
 checks "legacy entries in shadow" "0" "${plegshad}"
 
 # Check no legacy + entries in group
-pleggro=$(grep '^\+:' /etc/group | awk ' { print $1} ')
+pleggro=$(grep '^\+:' /etc/group )
 checks "legacy entries in group" "0" "${pleggro}"
 
 # Check root is the only UID 0 account
