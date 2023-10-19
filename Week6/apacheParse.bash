9b2e74e6d69c0ba84462b60dbece108b8101dcd6
LiamHarrison25 <liam.harrison@mymail.champlain.edu>
2023-10-18 23:20:12 -0400

    Updated apacheParse.bash

--- Week6/apacheParse.bash
+++ Week6/apacheParse.bash
@@ -18,12 +18,11 @@ sed -e "s/\[//g" -e "s/\"//g" ${APACHE_LOG} | \
 
 # Look for web scanners
 
-egrep -i "test|shell|echo|passwd|select|phpmyadmin|setup|admin|w00t" | \
+egrep -i "test|shell|echo|passwd|select|phpmyadmin|setup|admin|w00t" | sort -u | \
 
 awk ' BEGIN { format = "%-15s %-20s %-7s %-6s %-10s %s\n"
 	printf format, "IP", "Date", "Method", "Status", "Size", "URI"
 	printf format, "--", "----", "------", "------", "----", "---"}
 
 { printf format, $1, $4, $6, $9, $10, $7}'
-
 
