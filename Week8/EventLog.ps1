6e90fa5d0d6b55d166d12f151b4a2bc6335f8912
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-25 15:14:28 -0400

    Fixed the issue with the search
    The search will now correctly find indexes correctly with the inputted string

--- Week8/EventLog.ps1
+++ Week8/EventLog.ps1
@@ -6,9 +6,12 @@ Get-EventLog -list
 #Create a prompt to allow the user to select the log they want to view from the list
 $input = Read-Host -Prompt "Please select a log to view from the list above"
 
+#Create a promt to allow the user to select the search they want to use
+$searchInput = Read-Host -Prompt "Please select the string you want to search for"
+
 # Print the log corresponding to the user input
-Get-EventLog -LogName $input -Newest 40 | where {$_.Message -ilike "*new process has been*" } | Export-Csv -NoTypeInformation `
--Path "D:\Projects\Powershell Projects\CSI-230\Week8\Logs.csv"
+Get-EventLog -LogName $input -Newest 40 | where {$_.Message -ilike "*$searchInput*" } | Export-Csv -NoTypeInformation `
+-Path "D:\Projects\Powershell Projects\CSI-230\CSI-230\Week8\Logs.csv"
 
 
 
