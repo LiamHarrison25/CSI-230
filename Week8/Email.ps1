9d62c1196de71e7f2c097e17d3969a7e71830219
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-26 17:28:39 -0400

    Added the email
    Added the email.ps1

---
+++ Week8/Email.ps1
@@ -0,0 +1,16 @@
+﻿# Send an email
+
+#Email body
+$msg = "Please open the attached file for free gift cards!"
+
+#Writes the email to the screen
+write-host -BackgroundColor Red -ForegroundColor white $msg
+
+# Sender Email Address
+$email = "liam.harrison@mymail.champlain.edu"
+
+# Receiver Email Address
+$toEmail = "deployer@csi-web"
+
+#Sending email
+Send-MailMessage -From $email -to $toEmail -Subject "Free Gift Cards" -body $msg -SmtpServer 192.168.4.22
