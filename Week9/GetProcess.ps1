eb985a64c915fbd658295288bbf2195434f13de6
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-10-26 17:29:02 -0400

    Worked on the Week 9 assignment
    Started working on the week 9 assignment

---
+++ Week9/GetProcess.ps1
@@ -0,0 +1,6 @@
+﻿# Using Get-Process and Get-Service
+
+#Get-Process | Select-Object ProcessName, Path, ID | `
+#Export-Csv -Path "D:\Projects\Powershell Projects\CSI-230\CSI-230\Week9\myProcess.csv"
+
+Get-Process | Get-Member
