3262078286e87eaa78eb1ef033d429067ee56e1d
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-11-02 13:14:33 -0400

    Got the DHCP working

---
+++ Week9/DHCP.ps1
@@ -0,0 +1,7 @@
+﻿#Use the Get-WmiObject cmdlet
+# Get the DHCP server's IP address and the DNS server IPs
+
+
+ipconfig /all | Select-String -Pattern "DHCP Server"
+
+ipconfig /all | Select-String -Pattern "\d\.\d\.\d\.\d"
