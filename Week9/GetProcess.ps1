382a528ceb13d0a8cef4186e57f1aafdfe40ddc7
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-11-01 21:45:33 -0400

    Worked on the week 9 assignment
    Got the calculator and getProcess working.

--- Week9/GetProcess.ps1
+++ Week9/GetProcess.ps1
@@ -1,6 +1,7 @@
 ﻿# Using Get-Process and Get-Service
 
-#Get-Process | Select-Object ProcessName, Path, ID | `
-#Export-Csv -Path "D:\Projects\Powershell Projects\CSI-230\CSI-230\Week9\myProcess.csv"
+Get-Process | Select-Object ProcessName, Path, ID | `
+Export-Csv -Path "D:\Projects\Powershell Projects\CSI-230\CSI-230\Week9\myProcess.csv"
 
-Get-Process | Get-Member
+Get-Service | Where { $_.Status -eq "Running" } | `
+Export-Csv -Path "D:\Projects\Powershell Projects\CSI-230\CSI-230\Week9\myServices.csv"
---
+++ Week9/WMI.ps1
@@ -0,0 +1,17 @@
+﻿#Use the Get-WmiObject cmdlet
+# Get the DHCP server's IP address and the DNS server IPs
+
+
+
+
+#Get-WmiObject -Class Win32_service | select Name, PathName, ProcessID 
+
+#Get-WmiObject -list | where {$_.Name -ilike "Win32*" } | Sort-Object
+
+#Get-WmiObject -Class Win32_Account | Get-Member
+
+
+
+
+#Get-WmiObject -Class 
+
