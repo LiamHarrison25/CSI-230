17eeebd2e89d84bc23059d898eb0b58fb971933e
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-11-09 13:50:21 -0500

    Figured out which extra cmdlets to use

---
+++ Week11/IncidentResponseToolkit.ps1
@@ -0,0 +1,17 @@
+﻿# Write a script that retrieves:
+# 1: Running Processes and the path for each project
+# 2: All registered services and the path to the excecutable controlling the service (Use WMI)
+# 3: All TCP network sockets
+# 4: All user account information (use WMI)
+# 5: All network adapter configuration information
+# 6: use powershell cmdlets to save 4 other artifacts that would be useful in an incident. 
+
+
+#extra cmdlet ideas:
+
+# Stop-Computer //shuts down the computer
+# Stop-Process or Stop-Service //stops a process. Can be used to stop an "unsafe" application
+# Get-Job and Stop-Job //used to find and stop running powershell jobs
+# Remove-Item //used to delete an item. Can be used to delete an "unsafe" file (Create a file and then delete it)
+
+
