aba1465c2f8fd5cd7b364d79b4b6599b1e777a87
Liam Harrison <liam.harrison@mymail.champlain.edu>
2023-11-29 12:07:13 -0500

    Worked on week 12 ssh

---
+++ Week12/ssh.ps1
@@ -0,0 +1,36 @@
+﻿# Login to a remote SSH Server
+
+cls
+
+<#
+New-SSHSession -ComputerName '192.168.4.22' -Credential (Get-credential sys320)
+
+
+while ($CONDITION)
+
+{
+
+    #prompt to run commands
+    $command = read-host -Prompt "Please enter a command"
+
+    #run command on remote ssh server
+    (Invoke-SSHCommand -index 0 $command).Output
+
+}
+
+#>
+
+
+Set-SCPFile -ComputerName '192.168.4.22' -Credential (Get-credential sys320) `
+-RemotePath 'home/sys320' -LocalFile '.\tedx.jpeg' 
+
+
+
+
+
+
+
+
+
+# Close session
+Remove-SSHSession -SessionId 0
